97c33cf6dca08a9a253beccd44be1b43ad748e1f
e.deniku <e.deniku@trust-itservices.com>
2023-06-27 16:48:45 +0200

    Add sorting functionality to client index page. Validate client input data including uniqueness of the CF attribute. Display error message when client creation fails. This patch improves user experience by adding sorting functionality and displaying relevant error messages when client creation fails.

--- azienda-automobilistica/app/Http/Controllers/ClienteController.php
+++ azienda-automobilistica/app/Http/Controllers/ClienteController.php
@@ -7,10 +7,16 @@ use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
-    public function index()
-    {
-        return view('clienti.index')->with('clienti', Cliente::all());
-    }
+    public function index(Request $request)
+{
+    $sortColumn = $request->input('sort_by', 'CF');
+    $sortOrder = $request->input('sort_order', 'asc');
+
+    $clienti = Cliente::orderBy($sortColumn, $sortOrder)->get();
+
+    return view('clienti.index', compact('clienti'));
+}
+
 
     public function create()
     {
@@ -19,8 +25,22 @@ class ClienteController extends Controller
 
     public function store(Request $request)
     {
-        Cliente::create($request->all());
-        return redirect()->route('clienti.index')->with('message', 'Cliente creato con successo!');
+        $validatedData = $request->validateWithBag('clienti', [
+            'CF' => ['required', 'unique:clienti', 'max:16', 'min:16', 'alpha_num'],
+            'nome' => ['required', 'max:255'],
+            'cognome' => ['required', 'max:255'],
+            'data_nascita' => ['required', 'date', 'before:today', 'after:1900-01-01' , 'date_format:Y-m-d'],
+            'telefono' => ['required', 'numeric', 'digits_between:8,10'],
+            'buono_acquisto' => ['required', 'numeric', 'min:0',],
+        ]);
+
+        try {
+            Cliente::create($validatedData);
+        } catch (\Exception $e) {
+            return redirect()->route('clienti.index')->with('error', 'Errore durante la creazione del cliente!')
+                ->with('message', $e->getMessage());
+        }
+        return redirect()->route('clienti.index')->with('success', 'Cliente creato con successo!');
     }
 
     public function show(Cliente $cliente)
@@ -46,7 +66,14 @@ class ClienteController extends Controller
     }
 
 
-
+ //try to create a new Cliente else redirect to the index page with an error message
+        // try {
+        //     Cliente::create($request->all());
+        // } catch (\Exception $e) {
+        //     return redirect()->route('clienti.index')->with('error', 'Errore durante la creazione del cliente!')
+        //         ->with('message', $e->getMessage());
+        // }
+        // return redirect()->route('clienti.index')->with('success', 'Cliente creato con successo!');
 
 
 
