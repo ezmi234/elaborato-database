85ea0c55b57e84eeebe32a0419bda9aa375ea592
Fede <federica.guiducci3@studio.unibo.it>
2023-07-05 16:25:21 +0200

    meccanico

--- azienda-automobilistica/app/Http/Controllers/MeccanicoController.php
+++ azienda-automobilistica/app/Http/Controllers/MeccanicoController.php
@@ -16,4 +16,29 @@ class MeccanicoController extends Controller
     {
         return view('meccanici.create');
     }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validateWithBag('meccanici', [
+            'CF' => ['required', 'unique:meccanici', 'max:16', 'min:16', 'alpha_num'],
+            'nome' => ['required', 'max:255'],
+            'cognome' => ['required', 'max:255'],
+            'data_nascita' => ['required', 'date', 'before:today', 'after:1900-01-01' , 'date_format:Y-m-d'],
+            'telefono' => ['required', 'numeric', 'digits_between:8,10'],
+            'paga_oraria' => ['required', 'numeric', 'min:0',],
+            'totale_ore_svolte' => ['required', 'numeric', 'min:0',],
+            'bonus_recensione' => ['required', 'numeric', 'min:0',],
+            'media_recensione' => ['required', 'numeric', 'min:0', 'max:5',],
+        ]);
+
+        try {
+            Meccanico::create($validatedData);
+        } catch (\Exception $e) {
+            return redirect()->route('meccanici.index')->with('error', 'Errore durante la creazione del meccanico!')
+                ->with('message', $e->getMessage());
+        }
+        return redirect()->route('meccanici.index')->with('success', 'Meccanico creato con successo!');
+    }
+
+    
 }
---
+++ azienda-automobilistica/resources/views/meccanici/create.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@extends('layouts.sidebar')
+@section('content')
+
+
+<h1>Aggiungi Meccanico</h1>
+<form action="{{ route('meccanici.store') }}" method="POST">
+    @csrf
+    <div class="form-group" style="margin-bottom: 10px;">
+        <label for="CF">CF Meccanico</label>
+        <input type="text" name="CF" id="CF" class="form-control" placeholder="CF Meccanico" aria-describedby="helpId">
+    </div>
+    <div class="form-group" style="margin-bottom: 10px;">
+        <label for="nome">Nome Meccanico</label>
+        <input type="text" name="nome" id="nome" class="form-control" placeholder="Nome Meccanico" aria-describedby="helpId">
+    </div>
+    <div class="form-group" style="margin-bottom: 10px;">
+        <label for="cognome">Cognome Meccanico</label>
+        <input type="text" name="cognome" id="cognome" class="form-control" placeholder="Cognome Meccanico" aria-describedby="helpId">
+    </div>
+    <div class="form-group" style="margin-bottom: 10px;">
+        <label for="data_nascita">Data di nascita</label>
+        <input type="date" name="data_nascita" id="data_nascita" class="form-control" placeholder="Data di nascita" aria-describedby="helpId">
+    </div>
+    <div class="form-group" style="margin-bottom: 10px;">
+        <label for="telefono">Telefono</label>
+        <input type="text" name="telefono" id="telefono" class="form-control" placeholder="Telefono" aria-describedby="helpId">
+    </div>
+    <div class="form-group" style="margin-bottom: 10px;">
+        <label for="paga_oraria">Paga Oraria</label>
+        <input type="number" name="paga_oraria" id="paga_oraria" class="form-control" placeholder="Paga Oraria" aria-describedby="helpId">
+    </div>
+    <div class="form-group" style="margin-bottom: 10px;">
+        <label for="totale_ore_svolte">Totale Ore Svolte</label>
+        <input type="number" name="totale_ore_svolte" id="totale_ore_svolte" class="form-control" placeholder="Totale Ore Svolte" aria-describedby="helpId">
+    </div>
+    <div class="form-group" style="margin-bottom: 10px;">
+        <label for="bonus_recensione">Bonus Recensione</label>
+        <input type="number" name="bonus_recensione" id="bonus_recensione" class="form-control" placeholder="Bonus Recensione" aria-describedby="helpId">
+    </div>
+    <div class="form-group" style="margin-bottom: 10px;">
+        <label for="media_recensione">Media Recensione</label>
+        <input type="number" name="media_recensione" id="media_recensione" class="form-control" placeholder="Media Recensione" aria-describedby="helpId">
+    </div>
+    <button type="submit" class="btn btn-primary">Aggiungi Meccanico</button>
+</form>
+
+@if ($errors->any())
+ <div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+         <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+    </div>
+@endif
+
+@endsection
