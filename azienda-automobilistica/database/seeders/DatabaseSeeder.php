01712f232b04e07edd7460748430ce8fea922fa6
Ezmiron Deniku <ezmiron.deniku@studio.unibo.it>
2023-07-18 16:53:49 +0200

    Add codice_officina field to the Consulente and Meccanico models, create foreign key constraint to the officine table, and seed an officina record with code 1. This is necessary to associate consultants and mechanics with the respective workshops they work in.

--- azienda-automobilistica/database/seeders/DatabaseSeeder.php
+++ azienda-automobilistica/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            OfficinaSeeder::class,
             VeicoloSeeder::class,
             AccessorioSeeder::class,
             ClienteSeeder::class,
